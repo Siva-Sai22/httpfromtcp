2dbda69ae427ea752cfb5b02085094a71f2659c4
Siva Sai <22je0483@iitism.ac.in>
2026-01-10 00:25:06 +0530

    Add support for parsing multiple header lines

--- src/headers/mod.rs
+++ src/headers/mod.rs
@@ -12,27 +12,36 @@ impl Headers {
             Err(_) => return Err(error_malformed_header),
         };
 
-        let index = match request_string.find("\r\n") {
-            Some(i) => i,
-            None => return Ok((false, 0)),
-        };
+        let mut rest = request_string;
+        loop {
+            let index = match rest.find("\r\n") {
+                Some(i) => i,
+                None => return Ok((false, (request_string.len() - rest.len()))),
+            };
 
-        if index == 0 {
-            return Ok((true, "\r\n".len()));
+            if index == 0 {
+                return Ok((true, (request_string.len() - rest.len())));
+            }
+
+            let header_line = rest[..index].trim().to_string();
+
+            let seperator_index = match header_line.find(':') {
+                Some(i) => i,
+                None => return Err(error_malformed_header),
+            };
+
+            // TODO: Its better to use bytes only not String
+            if header_line.as_bytes()[seperator_index - 1] == b' ' {
+                return Err(error_malformed_header);
+            }
+
+            self.0.insert(
+                header_line[..seperator_index].to_string(),
+                header_line[(seperator_index + 1)..].trim().to_string(),
+            );
+
+            rest = &rest[index + 2..];
         }
-
-        let header_line = request_string[..index].trim().to_string();
-        // TODO: Its better to use bytes only not String
-        if header_line.as_bytes()[4] != b':' {
-            return Err(error_malformed_header);
-        }
-
-        self.0.insert(
-            header_line[..4].to_string(),
-            header_line[5..].trim().to_string(),
-        );
-
-        return Ok((true, index + 2));
     }
 
     fn new() -> Headers {
--- src/headers/test.rs
+++ src/headers/test.rs
@@ -23,3 +23,51 @@ fn invalid_spacing_header() {
 
     assert!(result.is_err());
 }
+
+#[test]
+fn valid_single_header_with_spaces() {
+    let mut headers = Headers::new();
+    let data = b"     Host:    localhost:42069    \r\n\r\n";
+
+    let result = headers.parse(data);
+    let (done, n) = result.unwrap();
+
+    assert!(!headers.0.is_empty());
+    assert_eq!(headers.0.get("Host"), Some(&"localhost:42069".to_string()));
+    assert_eq!(n, 35);
+    assert!(done);
+}
+
+#[test]
+fn valid_two_headers() {
+    let mut headers = Headers::new();
+    let data = b"Host: localhost:42069\r\nUser-Agent: TestAgent\r\n\r\n";
+
+    let result = headers.parse(data);
+    let (done, n) = result.unwrap();
+
+    assert!(!headers.0.is_empty());
+    assert_eq!(headers.0.get("Host"), Some(&"localhost:42069".to_string()));
+    assert_eq!(headers.0.get("User-Agent"), Some(&"TestAgent".to_string()));
+    assert_eq!(n, 46);
+    assert!(done);
+}
+
+#[test]
+fn valid_two_header_with_existing_headers() {
+    let mut headers = Headers::new();
+    headers
+        .0
+        .insert("Existing".to_string(), "Header".to_string());
+    let data = b"Host: localhost:42069\r\nUser-Agent: TestAgent\r\n\r\n";
+
+    let result = headers.parse(data);
+    let (done, n) = result.unwrap();
+
+    assert!(!headers.0.is_empty());
+    assert_eq!(headers.0.get("Existing"), Some(&"Header".to_string()));
+    assert_eq!(headers.0.get("Host"), Some(&"localhost:42069".to_string()));
+    assert_eq!(headers.0.get("User-Agent"), Some(&"TestAgent".to_string()));
+    assert_eq!(n, 46);
+    assert!(done);
+}
